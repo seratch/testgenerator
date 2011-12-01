fa82e261f9c8c0905a3ddced2d574ea556c09fdc
kaitenn <256.kaitenn@gmail.com>
2011-12-01 23:05:00 +0900

    added TestGenPlugin.

---
+++ core/src/main/scala/com/github/seratch/testgen/TestGenPlugin.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011 Kazuhiro SERA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.github.seratch.testgen
+
+import sbt.{ Command => SbtCommand, _ }
+import Path.sep
+import Keys._
+import java.io.File
+import ex.Ex._
+import com.github.seratch.testgen.{ Command => TestgenCommand }
+import util.Properties.propOrNone
+
+object TestGenKeys {
+  val genTest = InputKey[Unit]("gen-test")
+}
+
+object SbtIO {
+  def createFileIfNotExists(f: File, s: String): Unit = {
+    if(!f.exists) { IO.write(f, s) }
+    else { println(f + " already exists") }
+  }
+
+  def packToDir(s: String): String = s.replace(".", sep.toString)
+}
+
+object TestGenPlugin extends Plugin {
+  import TestGenKeys._
+  import SbtIO._
+
+  val generateTest = inputTask { (argTask: TaskKey[Seq[String]]) => 
+    (argTask, baseDirectory, sourceDirectory in Compile , sourceDirectory in sbt.Test, organization) map {
+      (args, base, main, test, org) => {
+        val orgDir = packToDir(org)
+        val targetFile: File = args.headOption.map { path => main / "scala" / orgDir / (path + ".scala") } getOrElse sys.error("path")
+        
+        val config = Config(propOrNone("encoding"), propOrNone("testTemplate"), propOrNone("scalatest.Matchers"), propOrNone("debug"))
+
+        val testGenerator = new TestGenerator(config)
+        val extractor = new TargetExtractor(config)
+
+  
+        val tests: Seq[Test] =  extractor.readFileAndExtractTargets(targetFile) map testGenerator.generate
+        
+        tests foreach { case Test(packageName, className, sourceCode) =>
+          val testFileDir = test / "scala" / (packToDir(packageName) + sep + (className + ".scala"))
+          createFileIfNotExists(testFileDir, sourceCode)
+        }
+      }
+    }
+  }
+
+  val testGenSettings = Seq(
+    genTest <<= generateTest
+  )
+}
